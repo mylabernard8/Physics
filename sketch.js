8f277322b8edc7997a2c9f4feb5a4ff02bedc7cc
mylabernard8 <myla.bernard26@avenues.org>
2025-04-09 20:42:03 -0400

    Adding multiple circles and making them bounce/ respond to one another

--- sketch.js
+++ sketch.js
@@ -5,7 +5,15 @@ function setup() {
   createCanvas(600, 400);
   ellipseMode(RADIUS);
   
-  circles.push(new Circle(300, 200, 20, 2, 0));
+  for (let i = 0; i < 5; i++) {
+    circles.push(new Circle(
+      random(50, width-50),
+      random(50, height-50),
+      20,
+      random(-2, 2),
+      random(-2, 2)
+    ));
+  }
 }
 
 function draw() {
@@ -16,6 +24,27 @@ function draw() {
     c.move();
     c.checkEdges();
     c.display();
+  }
+  
+  checkCollisions();
+}
+
+function checkCollisions() {
+  for (let i = 0; i < circles.length; i++) {
+    for (let j = i+1; j < circles.length; j++) {
+      let c1 = circles[i];
+      let c2 = circles[j];
+      let d = dist(c1.x, c1.y, c2.x, c2.y);
+      if (d < c1.r + c2.r) {
+        // Basic bounce: swap dx and dy
+        let tempDx = c1.dx;
+        let tempDy = c1.dy;
+        c1.dx = c2.dx;
+        c1.dy = c2.dy;
+        c2.dx = tempDx;
+        c2.dy = tempDy;
+      }
+    }
   }
 }
 
